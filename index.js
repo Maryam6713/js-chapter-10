5e2ccb5bc0a746a9754986b1384b16eaf0b9c515
Maryam6713 <maryammalik6713@gmail.com>
2024-05-12 18:34:36 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var studentAge = +prompt()
+console.log(studentAge)
+
+if(studentAge >= 18){
+    console.log("You are able to Apply")
+}else{
+    console.log("Sorry you are not able to Apply now!")
+}
+
+///------------------------------///
+
+var num = +prompt();
+
+if(num % 2 === 0){
+
+    console.log("The is even number!");
+}else if (num % 2 !== 0){
+    console.log('This is odd number!')
+}
+
+//------------------------------------//
